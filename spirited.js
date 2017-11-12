bc34cf45e016f6d57740105df46728d50e3bdf7c
sophiavanvalkenburg <sophiavanvalkenburg@users.noreply.github.com>
2017-11-11 21:14:14 -0500

    concentric squares dots

--- spirited.js
+++ spirited.js
@@ -6,13 +6,14 @@ function preload(){
 var lastTime = 0;
 var deltaTime = 0;
 var BOX_DIM = 30;
-var GRID_DENSITY = 3;
+var GRID_DENSITY = 1;
 var INTERVAL = BOX_DIM * GRID_DENSITY;
 var Z_SPEED = 5;
+var FRAME_MULTIPLIER = 0.2;
 var nCols, nRows;
 function setup(){
 
-  var cnv = createCanvas(800, 600, WEBGL);
+  var cnv = createCanvas(600, 600, WEBGL);
   nCols = Math.ceil(width / (GRID_DENSITY * BOX_DIM)) + 1;
   nRows = Math.ceil(height / (GRID_DENSITY * BOX_DIM)) + 1;
 
@@ -23,67 +24,76 @@ function setup(){
 
 }
 
-function draw(){
-  getDeltaTime();
-  var level = amplitude.getLevel();
-  var waveform = fft.waveform();
-  background(200);
-  drawCubeGrid(INTERVAL / 2);
-
-}
-
 function getDeltaTime(){
   var time = millis();
   deltaTime = time - lastTime;
   lastTime = time; 
 }
-/*
-function drawConcentricCubeSquares(){
-  var x = frameCount * 0.2;
-  drawRotatingCube(x);
-  for (var k = 1; k < nCircles; k++){
-    var sideLength = (2 * k + 1);
-    var startLoc = -(sideLength - 1) / 2;
-    translate(startLoc, 0, 0);
-    drawRotatingCube(x);
-    for (var j = 1; j < sideLength; j++){
-      translate(INTERVAL * j, 0, 0); 
-      drawRotatingCube(x);
 
-    }
+var GRID_FUNCTIONS = {
+  rightToLeftdiagonalWave: function(row, col, fc){
+    return sin(row + col + fc * FRAME_MULTIPLIER);
+  }, 
+  concentricSquaresWave: function(row, col, fc){
+    var squareLength = nRows < nCols ? nRows : nCols;
+    var row = row <= squareLength / 2 ? row : squareLength - row;
+    var col = col <= squareLength / 2 ? col : squareLength - col;
+    var val = row < col ? row : col;
+    return sin(val + fc * FRAME_MULTIPLIER);
   }
-}
-*/
+};
 
-function drawCubeGrid(offset){
+function drawGrid(offset, gridFn, drawObjFn){
   translate(-width / 2, -height / 2 + INTERVAL / 4, 0);
   for (var i = 0; i < nRows; i++){
     var startLoc = offset * (i % 2);
     translate(startLoc, 0, 0);
     for (var j = 0; j < nCols; j++){
-      //var x = Math.abs(nCols / 2 - j ) + Math.abs(nRows / 2 - i) + frameCount*0.2;
-      var x = j + i + frameCount * 0.2;
-      translate(INTERVAL * j, 0, -Z_SPEED * deltaTime * sin(x));
-      drawRotatingCube(x);
-      translate(-INTERVAL * j, 0, Z_SPEED * deltaTime * sin(x));
+      var t = gridFn(i, j, frameCount);
+      var c = setColors(t);
+      translate(INTERVAL * j, 0, -Z_SPEED * deltaTime * t);
+      drawObjFn(c);
+      translate(-INTERVAL * j, 0, Z_SPEED * deltaTime * t);
     }
     translate(-startLoc, INTERVAL, 0); 
   }
 }
 
-function drawRotatingCube(x){
-  push();
-  var red = map(sin(x), 1, -1, 255, 0);
-  var green = map(sin(x), 1, -1, 0, 200);
+
+function setColors(t){
+  var red = map(t, 1, -1, 255, 0);
+  var green = map(t, 1, -1, 0, 200);
   var blue = 128;
-  var alpha = map(sin(x), 1, -1, 50, 100);
-  stroke(0, 0, 0, alpha);
-  fill(red, green, blue, alpha)
+  var alpha = map(t, 1, -1, 50, 100);
+  return color(red, green, blue, alpha);
+}
+
+function drawRotatingCube(c){
+  stroke(0, 0, 0, c[3]);
+  fill(c);
+  push();
   rotateX(radians(45));
   rotateY(radians(45));
   rotate(radians(frameCount), [1, 1, -1])
   box(BOX_DIM, BOX_DIM, BOX_DIM);
   pop();
+}
+
+function drawDot(c){
+  noStroke();
+  fill(c);
+  push();
+  ellipse(0, 0, BOX_DIM, BOX_DIM);
+  pop();
+}
+
+function draw(){
+  getDeltaTime();
+  var level = amplitude.getLevel();
+  var waveform = fft.waveform();
+  background(200);
+  drawGrid(0, GRID_FUNCTIONS.concentricSquaresWave, drawDot, );
+
 }
 
 function togglePlay(){
